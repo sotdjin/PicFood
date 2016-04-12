14d290985fd0b161818adc3dcb871e57d9b7f39f
sotdjin <sotdjin@tamu.edu>
2016-04-11 19:55:30 -0500

    Made changes to syntax

--- FP_user.py
+++ FP_user.py
@@ -1,16 +1,17 @@
-class FP_user:
-    username=""
-    password=""
-    food_ratings={}
-    def __init__(self,username,password):
-        self.username=username
-        self.password=password
+class PFUser:
+    username = ""
+    password = ""
+    food_ratings = {}
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
         
     def display_user(self):
-        print "username:",self.username, "     password:", self.password
+        print "username:", self.username, "     password:", self.password
         
-    def add_ratings(self,key,rating):
-        self.food_ratings[key]=rating
+    def add_ratings(self, key, rating):
+        self.food_ratings[key] = rating
     
     def user_food_ratings(self):
         return self.food_ratings
--- picfood.py
+++ picfood.py
@@ -1,53 +1,55 @@
 import json
 import sys
+
+
 def main():
     data = []
     with open('yelp_academic_dataset_business.json') as f:
         for line in f:
             data.append(json.loads(line))
     print len(data)
-    restuarant_data=[]
+    restaurant_data = []
     for x in range(len(data)):
         if "Restaurants" in data[x]["categories"]:
-            restuarant_data.append(data[x])
-    print len(restuarant_data)
+            restaurant_data.append(data[x])
+    print len(restaurant_data)
     
     photo_data = []
     with open('photo_id_to_business_id.json') as f:
         photo_data = json.load(f)
-    kept_photos=[]
+    kept_photos = []
     print len(photo_data)
-    restuarant_data_indexed={}
-    for item in restuarant_data:
-            restuarant_data_indexed[item["business_id"]]=item
+    restaurant_data_indexed = {}
+    for item in restaurant_data:
+        restaurant_data_indexed[item["business_id"]] = item
     for item in photo_data:
-        if (item["business_id"])in restuarant_data_indexed:
+        if (item["business_id"]) in restaurant_data_indexed:
             kept_photos.append(item)
     print len(kept_photos)
     print "how many photos have labels and or captions"
-    count =0 
-    test_photos=[]
+    count = 0
+    test_photos = []
     for item in kept_photos:
-        if item["caption"]=="" and (item["label"]== "food" or item["label"]=="drink" or item["label"]=="none"):
-            count=count+1
+        if item["caption"] == "" and (item["label"] == "food" or item["label"] == "drink" or item["label"] == "none"):
+            count += 1
             test_photos.append(item["photo_id"])
     print count
-    file=open("photo_id_to_restuarant_NV.json",'w')
+    file = open("photo_id_to_restuarant_NV.json", 'w')
     file.write(json.dumps(kept_photos))
     file.close()
-    label_word={}
+    label_word = {}
     for item in kept_photos:
         if item["label"] not in label_word:
-            label_word[item["label"]]=1
-    label_file=open("unique labels.txt",'w')
+            label_word[item["label"]] = 1
+    label_file = open("unique labels.txt", 'w')
     label_file.write(json.dumps(label_word))
     label_file.close()
     
-    photo_id_file=open("photo_id.txt",'w')
+    photo_id_file = open("photo_id.txt", 'w')
     for x in range(10):
-        uri="C://2016_yelp_dataset_challenge_photos//" + test_photos[x] +".jpg 4:10"
-        photo_id_file.write(uri+ '\n')
+        uri = "C://2016_yelp_dataset_challenge_photos//" + test_photos[x] + ".jpg 4:10"
+        photo_id_file.write(uri + '\n')
     photo_id_file.close()
     
 if __name__ == '__main__':
-  main()
+    main()
