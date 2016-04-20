70ccb44b531555de792ae033d871bc09b54e626a
teahouse11 <austinktang@gmail.com>
2016-04-19 20:19:37 -0500

    web dev

--- web_dev/web_dev.py
+++ web_dev/web_dev.py
@@ -1,28 +1,46 @@
 # all the imports
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 from sqlalchemy import Column,Integer, Text
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///picfood.db'
-db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///picfood.db'
+db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = Column(Integer, primary_key=True)
-    username = Column(Text, unique= True)
-    password = Column(Text, unique= False)
+    username = Column(Text, unique=True)
+    password = Column(Text, unique=False)
 
 db.create_all()
 
-api_manager = APIManager(app,flask_sqlalchemy_db=db)
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
 api_manager.create_api(User, methods=['GET', 'Post', 'DELETE', 'PUT'])
+
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 app.debug=True
 
