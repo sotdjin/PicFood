385c3a105b36b14dd086f5ca953257c630c78db2
sotdjin <sotdjin@tamu.edu>
2016-04-20 03:08:27 -0500

    bugfixes on index.js

--- web_dev/static/js/index.js
+++ web_dev/static/js/index.js
@@ -8,16 +8,20 @@ $(document).ready(function() {
 
 function background_image() {
     var prevnumber = 0;
+    var number = 0;
     var imageStart = '../static/images/';
     var first_number = 1 + Math.floor(Math.random() * 5);
     imageStart = imageStart + "pic" + first_number.toString() + ".jpg";
     $("#indexbody").append('<img id="theimg" src=/' + imageStart + ' />');
     setInterval(function() {
+        prevnumber = number;
         var imageUrl = '../static/images/';
-        var number = 1 + Math.floor(Math.random() * 5);
-        imageUrl = imageUrl + "pic" + number.toString() + ".jpg";
-        $("#theimg").fadeOut(1000, function() {
-            $("#theimg").attr("src", imageUrl);
-        }).fadeIn(500);
+        number = 1 + Math.floor(Math.random() * 5);
+        if (prevnumber != number) {
+            imageUrl = imageUrl + "pic" + number.toString() + ".jpg";
+            $("#theimg").fadeOut(1000, function() {
+                $("#theimg").attr("src", imageUrl);
+            }).fadeIn(500);
+        }
     }, 5000);
 }
